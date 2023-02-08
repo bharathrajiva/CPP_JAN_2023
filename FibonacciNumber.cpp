c079a74a4343a591c4fa5092c7535f05ecc972d7
BHARATH RAJIV <bharathrajiva.aids2020@citchennai.net>
2023-02-08 08:05:20 +0530

    Feb 8 23

---
+++ FibonacciNumber.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Solution {
+    public:
+    int fib(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        int a=0;
+        int b=1;
+        int c;
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+
+    }};
+int main(){
+    int input;
+    cin>>input;
+    Solution s;
+    cout<<s.fib(input);
+    return 0;
+
+}
